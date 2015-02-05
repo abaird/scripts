727bb721d89abfbdb3ebcd8737999d0a29cc8864
vagrant <vagrant@lsdev.co>
2015-02-05 11:42:26 -0600

    the maximum command line length tmux takes is 2048 chars

--- start_sfl_tmx.sh
+++ start_sfl_tmx.sh
@@ -1,67 +1,105 @@
 #!/bin/sh
-WS=projects
+WS=~/projects
 
 if ! tmux has-session -t sfl; then
   tmux \
     new -s sfl -n browse \; \
     send-keys "cd $WS/browse" C-m \; \
     send-keys "BROWSE_SERVICE_LOCAL=true bundle exec foreman start" C-m \; \
+  detach
+  tmux \
     neww -n stepford \; \
-    send-keys "cd ~/projects/stepford" C-m \; \
+    send-keys "cd $WS/stepford" C-m \; \
     send-keys "STEPFORD_LOCAL=true bundle exec foreman start" C-m \; \
+  detach
+  tmux \
     neww -n sso-frontend \; \
-    send-keys "cd ~/projects/sso-frontend/" C-m \; \
+    send-keys "cd $WS/sso-frontend/" C-m \; \
     send-keys "STEPFORD_LOCAL=true bundle exec foreman start" C-m \; \
+  detach
+  tmux \
     neww -n accounts \; \
-    send-keys "cd ~/projects/accounts" C-m \; \
+    send-keys "cd $WS/accounts" C-m \; \
     send-keys "bundle exec foreman start" C-m \; \
+  detach
+  tmux \
     neww -n deals-service \; \
-    send-keys "cd ~/projects/deals-service" C-m \; \
+    send-keys "cd $WS/deals-service" C-m \; \
     send-keys "STEPFORD_LOCAL=true DEALS_SERVICE_HOST=http://deals-service.lsdev.co bundle exec foreman start" C-m \; \
+  detach
+  tmux \
     neww -n deals \; \
-    send-keys "cd ~/projects/deals" C-m \; \
+    send-keys "cd $WS/deals" C-m \; \
     send-keys "STEPFORD_LOCAL=true bundle exec foreman start" C-m \; \
+  detach
+  tmux \
     neww -n cupcake \; \
-    send-keys "cd ~/projects/cupcake" C-m \; \
+    send-keys "cd $WS/cupcake" C-m \; \
     send-keys "LOCAL_STEPFORD=true bundle exec foreman start" C-m \; \
+  detach
+  tmux \
     neww -n geo \; \
-    send-keys "cd ~/projects/geo" C-m \; \
+    send-keys "cd $WS/geo" C-m \; \
     send-keys "STEPFORD_LOCAL=true bundle exec foreman start" C-m \; \
+  detach
+  tmux \
     neww -n city-service \; \
-    send-keys "cd ~/projects/city-service" C-m \; \
+    send-keys "cd $WS/city-service" C-m \; \
     send-keys "STEPFORD_LOCAL=true bundle exec foreman start" C-m \; \
+  detach
+  tmux \
     neww -n browse-service \; \
-    send-keys "cd ~/projects/browse-service" C-m \; \
+    send-keys "cd $WS/browse-service" C-m \; \
     send-keys "bundle exec foreman start" C-m \; \
+  detach
+  tmux \
     neww -n pipeline \; \
-    send-keys "cd ~/projects/pipeline" C-m \; \
+    send-keys "cd $WS/pipeline" C-m \; \
     send-keys "bundle exec foreman start" C-m \; \
+  detach
+  tmux \
     neww -n deal_bucks \; \
-    send-keys "cd ~/projects/deal_bucks" C-m \; \
+    send-keys "cd $WS/deal_bucks" C-m \; \
     send-keys "bundle exec foreman start" C-m \; \
+  detach
+  tmux \
     neww -n fakeybiller \; \
-    send-keys "cd ~/projects/fakeybiller" C-m \; \
+    send-keys "cd $WS/fakeybiller" C-m \; \
     send-keys "bundle exec foreman start" C-m \; \
+  detach
+  tmux \
     neww -n payments \; \
-    send-keys "cd ~/projects/payments" C-m \; \
+    send-keys "cd $WS/payments" C-m \; \
     send-keys "bundle exec foreman start -f Procfile.no_fakeybiller" C-m \; \
+  detach
+  tmux \
     neww -n travel-service \; \
-    send-keys "cd ~/projects/travel-service" C-m \; \
+    send-keys "cd $WS/travel-service" C-m \; \
     send-keys "bundle exec foreman start" C-m \; \
+  detach
+  tmux \
     neww -n escapes \; \
-    send-keys "cd ~/projects/escapes" C-m \; \
+    send-keys "cd $WS/escapes" C-m \; \
     send-keys "STEPFORD_LOCAL=true bundle exec foreman start" C-m \; \
+  detach
+  tmux \
     neww -n products \; \
-    send-keys "cd ~/projects/products" C-m \; \
+    send-keys "cd $WS/products" C-m \; \
     send-keys "STEPFORD_LOCAL=true bundle exec foreman start" C-m \; \
+  detach
+  tmux \
     neww -n checkout \; \
-    send-keys "cd ~/projects/checkout" C-m \; \
+    send-keys "cd $WS/checkout" C-m \; \
     send-keys "STEPFORD_LOCAL=true bundle exec foreman start" C-m \; \
+  detach
+  tmux \
     neww -n sponsors \; \
-    send-keys "cd ~/projects/sponsors" C-m \; \
+    send-keys "cd $WS/sponsors" C-m \; \
     send-keys "STEPFORD_LOCAL=true bundle exec foreman start" C-m \; \
-    neww -n events
-    send-keys "cd ~/projects/events" C-m \; \
+  detach
+  tmux \
+    neww -n events \; \
+    send-keys "cd $WS/events" C-m \; \
     send-keys "STEPFORD_LOCAL=true bundle exec foreman start" C-m \; \
    detach 
 fi
